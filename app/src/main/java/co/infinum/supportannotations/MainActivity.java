60e79ce37b8a197332e4ed6bb0a39e76db871dfc
Josip Marković <josip.markovic@infinum.hr>
2016-03-13 22:14:45 +0100

    all annotation examples

--- app/src/main/java/co/infinum/supportannotations/MainActivity.java
+++ app/src/main/java/co/infinum/supportannotations/MainActivity.java
@@ -1,7 +1,40 @@
 package co.infinum.supportannotations;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Binder;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import static co.infinum.supportannotations.Utility.acceptRGBColor;
+import static co.infinum.supportannotations.Utility.accepts0to255;
+import static co.infinum.supportannotations.Utility.acceptsColorRes;
+import static co.infinum.supportannotations.Utility.acceptsFloat0To3;
+import static co.infinum.supportannotations.Utility.acceptsFloatNegative1to1Exclusive;
+import static co.infinum.supportannotations.Utility.acceptsIdRes;
+import static co.infinum.supportannotations.Utility.acceptsStringResource;
+import static co.infinum.supportannotations.Utility.doesNotAcceptNull;
+import static co.infinum.supportannotations.Utility.mayAcceptNull;
+import static co.infinum.supportannotations.Utility.mayReturnNull;
+import static co.infinum.supportannotations.Utility.neverReturnsNull;
+import static co.infinum.supportannotations.Utility.onBinderThread;
+import static co.infinum.supportannotations.Utility.onMainThread;
+import static co.infinum.supportannotations.Utility.onUIThread;
+import static co.infinum.supportannotations.Utility.onWorkerThread;
+import static co.infinum.supportannotations.Utility.requiresDangerousPermission;
+import static co.infinum.supportannotations.Utility.requiresGrantedPermission;
+import static co.infinum.supportannotations.Utility.requiresMultiplePermissions;
+import static co.infinum.supportannotations.Utility.requiresNonGrantedPermission;
+import static co.infinum.supportannotations.Utility.returnCheckResult;
+import static co.infinum.supportannotations.Utility.returnsAnimRes;
+import static co.infinum.supportannotations.Utility.returnsIdRes;
+import static co.infinum.supportannotations.Utility.returnsRGBColor;
+import static co.infinum.supportannotations.Utility.sizeAtLeast1;
+import static co.infinum.supportannotations.Utility.sizeAtMost5;
+import static co.infinum.supportannotations.Utility.sizeCombo;
+import static co.infinum.supportannotations.Utility.sizeExactly10;
+import static co.infinum.supportannotations.Utility.sizeMutlipleOf2;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +42,212 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //region NonNull and Nullable returns
+        // NonNull and Nullable returns
+        String maybeNull = mayReturnNull();
+        print(maybeNull.length()); // null warning, may crash app with NullPointerException
+
+        String neverNull = neverReturnsNull();
+        print(neverNull.length());
+
+        // NonNull and Nullable parameters
+        mayAcceptNull(null);
+
+        doesNotAcceptNull(null); // null warning
+        //endregion
+
+        //region android resources
+        // android resources
+        acceptsStringResource(R.string.app_name);
+
+        acceptsStringResource(R.id.tv_hello_world); // string resource expected, not ID
+
+        acceptsColorRes(R.color.colorPrimary);
+
+        acceptsColorRes(Color.RED); // it is color, but not color resource as expected
+
+        int id = returnsIdRes();
+        acceptsIdRes(id); // no warning
+
+        acceptsIdRes(returnsAnimRes()); // check goes through methods, not just variables
+        //endregion
+
+        //region threading
+        // threading
+        onMainThread();
+
+        onUIThread(); // both main and ui thread annotation work for the same thread as those are interchangeable
+
+        onWorkerThread(); // should not run on main thread
+
+        onBinderThread(); // should not run on main thread
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                onMainThread(); // may crash
+
+                onUIThread(); // will crash, no UI work available on any thread other than UI
+
+                onWorkerThread();
+
+                onBinderThread(); // still doesn't accept binder
+
+                return null;
+            }
+        };
+
+        /*todo NOTE binder class example at the bottom */
+
+        //endregion
+
+        //region threading on class
+        // threading on class
+        final Utility.ExampleUIThreadClass uiThreadObject = new Utility.ExampleUIThreadClass();
+        uiThreadObject.doesNotHaveUIThreadAnnotation();
+
+        final Utility.ExampleWorkerThreadClass workerThreadObject = new Utility.ExampleWorkerThreadClass();
+        workerThreadObject.doesNotHaveWorkerTheadAnnotation(); // still knows
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                workerThreadObject.doesNotHaveWorkerTheadAnnotation(); // now it works
+
+                uiThreadObject.doesNotHaveUIThreadAnnotation(); // it knows
+
+                return null;
+            }
+
+            @Override
+            protected void onProgressUpdate(Void... values) {
+                workerThreadObject.doesNotHaveWorkerTheadAnnotation();
+
+                uiThreadObject.doesNotHaveUIThreadAnnotation();
+            }
+        };
+        //endregion
+
+        //region RGB colors
+        // RGB colors
+        acceptRGBColor(Color.RED); // accepts color values instead color resources
+
+        acceptRGBColor(R.color.colorPrimary); // this is a resource, must be actual color
+
+        acceptRGBColor(ContextCompat.getColor(this, R.color.colorPrimary)); // resolves into a color
+
+        int color = returnsRGBColor();
+
+        acceptRGBColor(color); // works because it is a resolved color
+        //endregion
+
+        //region value constraints - ranges
+        // value constraints - ranges
+        accepts0to255(120);
+
+        accepts0to255(10000); // value out of range
+
+        acceptsFloat0To3(2);
+
+        acceptsFloat0To3(3);
+
+        acceptsFloat0To3(5); // value out of range
+
+        acceptsFloatNegative1to1Exclusive(0);
+
+        acceptsFloatNegative1to1Exclusive(1); // value is exclusive
+        //endregion
+
+        //region value constraints - array and list size (any length measurement, including String)
+        // value constraints - array and list size (any length measurement, including String)
+        sizeAtLeast1("Hello");
+
+        sizeAtLeast1(""); // size constraint not satisfied
+
+        sizeAtMost5("Hello");
+
+        sizeAtMost5("Hello World"); // size constraint not satisfied
+
+        sizeExactly10("HelloWorld"); // no space
+
+        sizeExactly10("Hello World"); // with space
+
+        sizeMutlipleOf2("HelloWorld"); // length is 10
+
+        sizeMutlipleOf2("Hello World"); // length is 11
+
+        sizeCombo("ElCapitan"); // 9 long
+
+        sizeCombo("Hello World"); // not multiple of 3
+
+        sizeCombo("Ice"); // not at least 6 long
+
+        sizeCombo("AndroidStudio20"); // 15 long, which is over 12
+        //endregion
+
+        //region permissions
+        // permissions
+        requiresGrantedPermission(); // set in manifest
+
+        requiresNonGrantedPermission(); // not set in manifest
+
+        requiresMultiplePermissions(); // multiple permissions not set in manifest
+
+        // generated check
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+        requiresDangerousPermission();
+        //endregion
+
+        //region Call super
+        // Call Super
+        new Utility.CallSuperExample() {
+            @Override
+            public void mustCallSuper() {
+                super.mustCallSuper();
+            }
+            @Override
+            public void doesNotNeedToCallSuper() {
+                // no op
+            }
+        };
+
+        new Utility.CallSuperExample() {
+            @Override
+            public void mustCallSuper() {
+                // no op
+            }
+        };
+        //endregion
+
+        //region check result
+        // Check result
+        returnCheckResult();
+
+        int result = returnCheckResult();
+        //endregion
+
+    }
+
+    /**
+     * Prints the String value of provided {@code object}.
+     */
+    private void print(Object object) {
+        System.out.println(String.valueOf(object));
+    }
+
+    public static class ExampleBinder extends Binder {
+        public void binderWork() {
+            onBinderThread(); // finally accepts binder
+        }
     }
 }
---
+++ app/src/main/java/co/infinum/supportannotations/Utility.java
@@ -0,0 +1,226 @@
+package co.infinum.supportannotations;
+
+import android.Manifest;
+import android.graphics.Color;
+import android.support.annotation.AnimRes;
+import android.support.annotation.BinderThread;
+import android.support.annotation.CallSuper;
+import android.support.annotation.CheckResult;
+import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
+import android.support.annotation.FloatRange;
+import android.support.annotation.IdRes;
+import android.support.annotation.IntRange;
+import android.support.annotation.Keep;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresPermission;
+import android.support.annotation.Size;
+import android.support.annotation.StringRes;
+import android.support.annotation.UiThread;
+import android.support.annotation.VisibleForTesting;
+import android.support.annotation.WorkerThread;
+
+import java.util.Random;
+
+/**
+ * Created by jmarkovic on 13/03/16.
+ */
+public final class Utility {
+
+    private static Random random = new Random();
+
+    private Utility() {
+        throw new AssertionError("cannot instantiate");
+    }
+
+    //region Nullable and NonNull
+    @Nullable
+    public static String mayReturnNull() {
+        return random.nextBoolean() ? "not null" : null;
+    }
+
+    @NonNull
+    public static String neverReturnsNull() {
+        return "this is never null";
+    }
+
+    public static void mayAcceptNull(@Nullable String nullableParam) {
+        // no op
+    }
+
+    public static void doesNotAcceptNull(@NonNull String nonNullParam) {
+        // no op
+    }
+    //endregion
+
+    //region resources
+    public static void acceptsStringResource(@StringRes int stringRes) {
+        // no op
+    }
+
+    public static void acceptsColorRes(@ColorRes int colorRes) {
+        // no op
+    }
+
+    @IdRes
+    public static int returnsIdRes() {
+        return R.id.tv_hello_world;
+    }
+
+    public static void acceptsIdRes(@IdRes int id) {
+        // no op
+    }
+
+    @AnimRes
+    public static int returnsAnimRes() {
+        return R.anim.no_op;
+    }
+    //endregion
+
+    //region threading
+    @MainThread
+    public static void onMainThread() {
+        // no op
+    }
+
+    @UiThread
+    public static void onUIThread() {
+        // no op
+    }
+
+    @WorkerThread
+    public static void onWorkerThread() {
+        // no op
+    }
+
+    @BinderThread
+    public static void onBinderThread() {
+        // no op
+    }
+    //endregion
+
+    //region RGB colors
+    public static void acceptRGBColor(@ColorInt int colorInt) {
+
+    }
+
+    @ColorInt
+    public static int returnsRGBColor() {
+        return Color.RED;
+    }
+    //endregion
+
+    //region value constraints
+    public static void accepts0to255(@IntRange(from = 0, to = 255) int value) {
+        // no op
+    }
+
+    public static void acceptsFloat0To3(@FloatRange(from = 0, to = 3) float value) {
+        // no op
+    }
+
+    public static void acceptsFloatNegative1to1Exclusive(
+            @FloatRange(from = -1f, fromInclusive = false, to = 1f, toInclusive = false) int value) {
+        // no op
+    }
+
+    public static void sizeAtLeast1(@Size(min = 1) String text) {
+        // no op
+    }
+
+    public static void sizeAtMost5(@Size(max = 5) String text) {
+        // no op
+    }
+
+    public static void sizeExactly10(@Size(10) String text) {
+        // no op
+    }
+
+    public static void sizeMutlipleOf2(@Size(multiple = 2) String text) {
+        // no op
+    }
+
+    public static void sizeCombo(@Size(min = 6, max = 12, multiple = 3) String text) {
+        // no op
+    }
+    //endregion
+
+    //region permissions
+    @RequiresPermission(Manifest.permission.CAMERA)
+    public static void requiresNonGrantedPermission() {
+        // no op
+    }
+
+    @RequiresPermission(Manifest.permission.ACCESS_WIFI_STATE)
+    public static void requiresGrantedPermission() {
+        // no op
+    }
+
+    @RequiresPermission(allOf = { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE })
+    public static void requiresMultiplePermissions() {
+        // no op
+    }
+
+    @RequiresPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    public static void requiresDangerousPermission() {
+        // no op
+    }
+
+    // any of and Read/Write are used so rarely they don't even deserve the example
+    // show from sources
+    //endregion
+
+    //region check result
+    public static void doingSomethingWithResult(int value) {
+        // no op
+    }
+
+    @CheckResult(suggest = "#doingSomethingWithResult")
+    public static int returnCheckResult() {
+        return random.nextInt();
+    }
+    //endregion
+
+    //region visible for testing
+    @VisibleForTesting // also applicable to classes and fields
+    public static int returnValueOnlyForTests() {
+        return random.nextInt();
+    }
+    //endregion
+
+    //region keep
+    @Keep // also applicable to classes
+    public static void keepThisMethodWhenMinimizing() {
+        // no op
+    }
+    //endregion
+
+
+    //region helper classes
+    @UiThread
+    public static class ExampleUIThreadClass {
+        public void doesNotHaveUIThreadAnnotation() {
+            // no op
+        }
+    }
+
+    @WorkerThread
+    public static class ExampleWorkerThreadClass {
+        public void doesNotHaveWorkerTheadAnnotation() {
+            // no op
+        }
+    }
+
+    public static class CallSuperExample {
+        @CallSuper
+        public void mustCallSuper() {
+            // no op
+        }
+        public void doesNotNeedToCallSuper() {
+            // no op
+        }
+    }
+    //endregion
+}
